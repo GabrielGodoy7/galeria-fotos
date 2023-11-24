0117e1438e6a06fee441627a5a3e18f426f0d8c5
GabrielGodoy7 <142518644+GabrielGodoy7@users.noreply.github.com>
2023-11-23 22:02:18 -0300

    conclusao galeria fotos

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+  
+  constructor(public photoService: PhotoService) { }
+  
 
-  constructor() {}
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 
 }
